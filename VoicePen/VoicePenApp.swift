b6313577a6a7de76556dfe72e50640ba80e7b534
Sergey Khokhlachev <sergey@khokhlachev.com>
2026-05-01 16:31:00 +0400

    Initial Commit

---
+++ VoicePen/VoicePenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicePenApp.swift
+//  VoicePen
+//
+//  Created by Sergey Khokhlachev on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct VoicePenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
